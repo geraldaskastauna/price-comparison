67c02670b3d99f9aa63d04e921ef6b1400728200
geraldaskastauna <gk468@live.mdx.ac.uk>
2019-12-03 18:58:53 +0000

    Fixing hibernate

--- webscraping/src/main/java/webscraping/coursework/Main.java
+++ webscraping/src/main/java/webscraping/coursework/Main.java
@@ -1,6 +1,5 @@
 package webscraping.coursework;
 
-import java.util.Scanner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
 /**
@@ -9,15 +8,6 @@ import org.springframework.context.annotation.*;
  */
 public class Main {
     public static void main(String[] args) { 
-        /* ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        
-        Laptop laptop = (Laptop) context.getBean("Laptop");
-        
-        System.out.println(laptop.getBrand());
-        laptop.setBrand("WHATEVER");
-        System.out.println(laptop.getBrand());
-        */
-        /*
         LaptopsDirectScraper laptopsDirect = new LaptopsDirectScraper();
         EBuyerScraper eBuyer = new EBuyerScraper();
         VeryScraper very = new VeryScraper();
@@ -29,16 +19,6 @@ public class Main {
         very.start();
         laptopOutlet.start();
         box.start();
-        
-        Scanner input = new Scanner(System.in);
-        if(input.next() == "stop"){
-        //Stop threads
-        laptopsDirect.stopThread();
-        eBuyer.stopThread();
-        very.stopThread();
-        laptopOutlet.stopThread();
-        box.stopThread();
-        }
         
         //Wait for threads to finish - join can throw an InterruptedException
         try{
@@ -54,9 +34,6 @@ public class Main {
         
         System.out.println("Web scraping complete");
         System.out.println("END OF PROGRAM");
-*/
         
-        Hibernate example = new Hibernate();
-        example.init();
     }
 }
