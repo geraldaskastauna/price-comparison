f572ff27891f14173ec4fb37847c1fd2b638a8e5
geraldaskastauna <gk468@live.mdx.ac.uk>
2019-12-04 09:02:15 +0000

    Beans

--- webscraping/src/main/java/webscraping/coursework/Main.java
+++ webscraping/src/main/java/webscraping/coursework/Main.java
@@ -2,16 +2,18 @@ package webscraping.coursework;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
+
 /**
- *
- * @author linux
+ * Contains main method for application
  */
+
 public class Main {
     public static void main(String[] args) { 
-        runThreads();
+        runThreadsAnnotations();
     }
     
-    static void runThreads(){
+    /** Uses Spring Annotation configuration to set up and run application */
+    static void runThreadsAnnotations(){
             
     //Instruct Spring to create and wire beans using XML file
     ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
